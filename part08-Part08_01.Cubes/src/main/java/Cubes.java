fcc0b33f33c3421a49635c34a4d27d8c456c6d31
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-09-27 21:40:15 +0800

    completed cube exercise

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -6,5 +6,17 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+
+
+            int numInput = Integer.valueOf(input);
+            System.out.println(numInput * numInput * numInput);
+        }
+
     }
 }
