dc9885b1b0afbb31181aeb4cdadee728efbb8d29
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-09-28 22:40:58 +0800

    added AverageOfPositiveNumbers

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -1,10 +1,31 @@
-
 import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int positiveNumCounter = 0;
+        int sum = 0;
+
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == 0) {
+                break;
+            }
+
+            if (input > 0) {
+                sum += input;
+                positiveNumCounter++;
+            }
+        }
+
+        if (positiveNumCounter == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println(sum / (double) positiveNumCounter);
+        }
 
     }
 }
