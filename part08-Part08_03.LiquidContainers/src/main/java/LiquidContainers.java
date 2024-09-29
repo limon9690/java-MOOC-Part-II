29e68f9fdff738118967e5df6926cd9375c6178d
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-09-29 13:03:34 +0800

    LiquidContainers

--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,14 +5,67 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int first = 0;
+        int second = 0;
+        int capacity = 100;
 
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: " + first + "/" + capacity);
+            System.out.println("Second: " + second + "/" + capacity);
 
             String input = scan.nextLine();
+
             if (input.equals("quit")) {
                 break;
+            }
+
+            String[] parts = input.split(" ");
+            String cmd = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+
+            // Move Amount
+            if (cmd.equals("move")) {
+                if (amount < 0) continue;
+
+                if (amount > first) {
+                    if (second + first > capacity) {
+                        second = capacity;
+                    } else {
+                        second += first;
+                    }
+                    first = 0;
+                } else {
+                    second += amount;
+                    first -= amount;
+                    if (second > capacity) second = capacity;
+                }
+            }
+
+            
+            // Remove Amount
+            if (cmd.equals("remove")) {
+                if (amount < 0) continue;
+
+                if (amount > second) {
+                    second = 0;
+                } else {
+                    second -= amount;
+                }
+
+            }
+
+
+            // Add Amount
+            if (cmd.equals("add")) {
+                if (amount > 0) {
+                    if (first + amount > capacity) {
+                    first = capacity;
+                    } else {
+                    first += amount;
+                    }
+                }
             }
 
         }
