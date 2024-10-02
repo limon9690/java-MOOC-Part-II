f77449bb161c09cbf22056bc466be90e8aebdf7c
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-10-02 14:06:21 +0800

    finished whole exercise - StorageFacility

--- part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
+++ part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
@@ -8,6 +8,35 @@ public class StorageFacility {
         this.directory = new HashMap<>();
     }
 
+    public ArrayList<String> storageUnits() {
+        ArrayList<String> result = new ArrayList<>();
+        
+        for (String key : this.directory.keySet()) {
+            result.add(key);
+        }
+        
+        return result;
+    }
+
+    public void remove(String storageUnit, String item) {
+        if (!this.directory.containsKey(storageUnit)) {
+            return;
+        }
+
+        ArrayList<String> list = this.directory.get(storageUnit);
+
+        for (String s : list) {
+            if (s.equals(item)) {
+                list.remove(s);
+                break;
+            }
+        }
+
+        if (this.directory.get(storageUnit).isEmpty()) {
+            this.directory.remove(storageUnit);
+        }
+    }
+
     public void add(String unit, String item) {
         this.directory.putIfAbsent(unit, new ArrayList<>());
         this.directory.get(unit).add(item);
