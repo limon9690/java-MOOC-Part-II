6476cd7db33e2935bf6fc9230ecce7f3947d7415
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-10-04 19:53:42 +0800

    added exercise - 'SetAsMethodParameter'

--- part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
+++ part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
@@ -1,4 +1,5 @@
-
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
@@ -9,5 +10,9 @@ public class Main {
 
     // implement the method returnSize here, which returns
     // the number of elements in the set that it receives as a parameter.
+
+    public static int returnSize(Set<Object> set) {
+        return set.size();
+    }
 
 }
