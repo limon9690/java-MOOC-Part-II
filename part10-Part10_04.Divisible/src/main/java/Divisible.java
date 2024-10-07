aab9e820d9df607920f0fb9df4f7f7f942429328
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-10-07 17:49:36 +0800

    added exercise - divisble

--- part10-Part10_04.Divisible/src/main/java/Divisible.java
+++ part10-Part10_04.Divisible/src/main/java/Divisible.java
@@ -19,7 +19,11 @@ public class Divisible {
     }
 
     public static ArrayList<Integer> divisible(ArrayList<Integer> numbers) {
-        return new ArrayList<>();
+        ArrayList<Integer> result = numbers.stream()
+                                    .filter(num -> (num % 2 == 0 || num % 3 == 0 || num % 5 == 0))
+                                    .collect(Collectors.toCollection(ArrayList::new));
+
+        return result;
     }
 
 }
