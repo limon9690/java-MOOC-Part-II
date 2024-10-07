6d515c31ec6fc2d59595cec84737d0a93dccf29f
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-10-07 17:21:41 +0800

    added exercise - AverageOfPositive&Negative

--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,12 +1,42 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        List<String> list = new ArrayList<>();
+
+        while (true) {
+            String row = scanner.nextLine();
+
+            if (row.equals("end")) {
+                break;
+            }
+
+            list.add(row);
+        }
+
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String cmd = scanner.nextLine();
+
+        if (cmd.equals("p")) {
+            double averageP = list.stream()
+                              .mapToInt(num -> Integer.valueOf(num))
+                              .filter((num) -> num > 0)
+                              .average()
+                              .getAsDouble();
+            System.out.println("Average of the positive numbers: " + averageP);
+        } else {
+            double averageN = list.stream()
+                              .mapToInt(num -> Integer.valueOf(num))
+                              .filter(num -> num < 0)
+                              .average()
+                              .getAsDouble();
+            System.out.println("Average of the positive numbers: " + averageN);
+        }
 
     }
 }
