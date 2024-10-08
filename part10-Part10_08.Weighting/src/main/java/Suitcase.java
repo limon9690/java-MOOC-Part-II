830acf02d44060cb6f330e2bb6af29a5a5c6edc5
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-10-08 16:37:34 +0800

    cleaned for-loop/while loop and replaced with streams of exercise - Weighting

--- part10-Part10_08.Weighting/src/main/java/Suitcase.java
+++ part10-Part10_08.Weighting/src/main/java/Suitcase.java
@@ -20,22 +20,15 @@ public class Suitcase {
     }
 
     public int totalWeight() {
-        int summa = 0;
-        int indeksi = 0;
-        while (indeksi < this.Items.size()) {
-        summa += this.Items.get(indeksi).getweight();
-        indeksi++;
-        }
-        return summa;
+        int sum =  this.Items.stream()
+                  .mapToInt(item -> item.getWeight())
+                  .sum(); 
+        return sum;
     }
 
     public void printItems() {
-        int indeksi = 0;
-        while (indeksi < this.Items.size()) {
-        Item t = this.Items.get(indeksi);
-        System.out.println(t);
-        indeksi++;
-        }
+        this.Items.stream()
+                  .forEach(item -> System.out.println(item));
     }
 
     public Item heaviestItem() {
