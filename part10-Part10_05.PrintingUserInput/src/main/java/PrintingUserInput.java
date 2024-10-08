e3d18a48caf993625b609f186c119661942177c0
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-10-08 16:21:28 +0800

    added exercise - PrintingUserInputTest

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -1,11 +1,27 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        List<String> list = new ArrayList<>();
+
+        while (true) {
+            String read = scanner.nextLine();
+
+            if (read.equals("")) {
+                break;
+            }
+
+            list.add(read);
+        }
+
+        list.stream()
+            .forEach(s -> System.out.println(s));
 
     }
 }
