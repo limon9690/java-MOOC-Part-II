2a7105b16f93cefb3b9ad37ab01556cbf47cefa4
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-10-08 16:24:29 +0800

    added exercise - LimitedNumbers

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,28 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        List<Integer> list = new ArrayList<>();
+
+        while (true) {
+            int value = Integer.valueOf(scanner.nextLine());
+
+            if (value < 0) {
+                break;
+            }
+
+            list.add(value);
+        }
+
+        list.stream()
+            .filter(val -> (val >= 1 && val <= 5))
+            .forEach(val -> System.out.println(val));
 
     }
 }
