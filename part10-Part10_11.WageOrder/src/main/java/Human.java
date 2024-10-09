3228f1d27925ad2a02a6be3ffa5a7080f6c79b58
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-10-09 18:13:48 +0800

    added exercise - WageOrder

--- part10-Part10_11.WageOrder/src/main/java/Human.java
+++ part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -1,5 +1,5 @@
 
-public class Human {
+public class Human implements Comparable<Human>{
 
     private int wage;
     private String name;
@@ -22,4 +22,11 @@ public class Human {
     public String toString() {
         return name + " " + wage;
     }
+
+    @Override
+    public int compareTo(Human human) {
+        return human.getWage() - this.wage;
+    }
+
+
 }
