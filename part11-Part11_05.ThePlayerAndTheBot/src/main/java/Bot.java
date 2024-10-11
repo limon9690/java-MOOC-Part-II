1d4612a2e3e7bea964d89bc35638a732989d23ef
TheCyberSavior01 <limon.hossain26@yahoo.com>
2024-10-11 14:35:46 +0800

    added exercise - PlayerAndBot

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,11 @@
+public class Bot extends Player{
+
+
+    public void play () {
+        System.out.println("Bot plays");
+    }
+
+    public void addMove() {
+        System.out.println("move added to bot");
+    }
+}
---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+    private String name;
+
+
+    public void play () {
+        System.out.println(this.name + " plays.");
+    }
+
+    public void printName() {
+        System.out.println(this.name);
+    }
+}
