a9b82f0ddad15e8524299e9c33105eb3823453a0
limon9690 <limon.hossain26@yahoo.com>
2024-10-19 14:06:16 +0800

    added exercise - Numbers

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,15 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many random numbers should be printed?");
+        int cmd = Integer.valueOf(scanner.nextLine());
+
+        Random rand = new Random();
+
+        for (int i = 0; i < cmd; i++) {
+            System.out.println(rand.nextInt(11));
+        }
     }
 
 }
