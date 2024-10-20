d38c8a2d88fe883722d16b31a0ea9ea09ac8401c
limon9690 <limon.hossain26@yahoo.com>
2024-10-20 22:01:55 +0800

    added exercise - ArrayAsString

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -5,4 +5,17 @@ public class Program {
         // Test your method here
     }
 
+    public static String arrayAsString(int[][] array) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[row].length; col++) {
+                sb.append(array[row][col]);
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
 }
